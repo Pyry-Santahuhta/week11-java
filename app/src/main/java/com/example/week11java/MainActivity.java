d4f604039d42bd1d2806eac3081dcf8fc8feb37f
Pyry-Santahuhta <61690808+Pyry-Santahuhta@users.noreply.github.com>
2020-04-29 15:37:28 +0300

    29.4 15.37

--- app/src/main/java/com/example/week11java/MainActivity.java
+++ app/src/main/java/com/example/week11java/MainActivity.java
@@ -1,43 +1,64 @@
 package com.example.week11java;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    Button navigationDrawerButton;
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        navigationDrawerButton = findViewById(R.id.BurgerMenu);
-        View.OnClickListener listener = new View.OnClickListener() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Fragment fragment;
-
-                if (v == findViewById(R.id.BurgerMenu)){
-                    fragment = new NavigationDrawerFragment();
-                }
-                else {
-                    fragment = new NavigationDrawerFragment();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.settings) {
+                    loadActivity();
                 }
 
 
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.replace(R.id.navigationDrawer, fragment);
-                transaction.commit();
-
-
+                return true;
             }
-        };
-        navigationDrawerButton.setOnClickListener(listener);
+        });
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(drawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void loadActivity(){
+        Intent intent = new Intent((Activity)MainActivity.this, settingsActivity.class);
+        startActivity(intent);
     }
 }
