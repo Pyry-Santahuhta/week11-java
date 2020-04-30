a1d9dd35aecae9485b7bbf512e1db1208c648f43
Pyry-Santahuhta <61690808+Pyry-Santahuhta@users.noreply.github.com>
2020-04-30 15:42:40 +0300

    Displaytext 29.4 15.37

--- app/src/main/java/com/example/week11java/MainActivity.java
+++ app/src/main/java/com/example/week11java/MainActivity.java
@@ -5,29 +5,38 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     private NavigationView navigationView;
+    TextView displayText;
+    String displayTextString;
 
 
+    int fontSize = 14;
+    int textWidth;
+    int textHeight;
+    int lineCount = 7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawerLayout = findViewById(R.id.drawer_layout);
-
-
+        displayText = findViewById(R.id.displayTextView);
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) displayText.getLayoutParams();
+        textHeight = params.height;
+        textWidth = params.width;
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
@@ -58,7 +67,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadActivity(){
-        Intent intent = new Intent((Activity)MainActivity.this, settingsActivity.class);
-        startActivity(intent);
+        Intent intent = new Intent(MainActivity.this, settingsActivity.class);
+        intent.putExtra("Size", fontSize);
+        intent.putExtra("Width", textWidth);
+        intent.putExtra("Height", textHeight);
+        intent.putExtra("lineCount", lineCount);
+        startActivityForResult(intent, 1);
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1){
+            if (resultCode == RESULT_OK ){
+                fontSize = data.getIntExtra("Size", -1);
+                textWidth = data.getIntExtra("Width", -1);
+                textHeight = data.getIntExtra("Height", -1);
+                lineCount = data.getIntExtra("lineCount", -1);
+                displayTextString = data.getStringExtra("displayText");
+                displayText.setText(displayTextString);
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) displayText.getLayoutParams();
+                params.width = textHeight;
+                params.height = textHeight;
+                displayText.setTextSize(fontSize);
+                displayText.setLines(lineCount);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/week11java/settingsActivity.java
+++ app/src/main/java/com/example/week11java/settingsActivity.java
@@ -2,13 +2,40 @@ package com.example.week11java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class settingsActivity extends AppCompatActivity {
+
+    EditText fontSize, boxWidth, boxHeight, lineCount, displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        fontSize = findViewById(R.id.fontSizeInt);
+        boxWidth = findViewById(R.id.textBoxWidthInt);
+        boxHeight = findViewById(R.id.textBoxHeightInt);
+        lineCount = findViewById(R.id.lineCountInt);
+        displayText = findViewById(R.id.displayTextString);
+        Intent intent = getIntent();
+        fontSize.setText(String.valueOf(intent.getIntExtra("Size", -1)));
+        boxWidth.setText(String.valueOf(intent.getIntExtra("Width", -1)));
+        boxHeight.setText(String.valueOf(intent.getIntExtra("Height", -1)));
+        lineCount.setText(String.valueOf(intent.getIntExtra("lineCount", -1)));
+
     }
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent();
+        intent.putExtra("Size", Integer.parseInt(fontSize.getText().toString()));
+        intent.putExtra("Width", Integer.parseInt(boxWidth.getText().toString()));
+        intent.putExtra("Height", Integer.parseInt(boxHeight.getText().toString()));
+        intent.putExtra("lineCount", Integer.parseInt(lineCount.getText().toString()));
+        intent.putExtra("displayText", displayText.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
 }
